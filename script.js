fdb2278c79c29ac642a8abdf7afb536f0a9a83b5
kevinkamau <kevin.kamau2@student.moringaschool.com>
2024-07-17 21:47:13 +0300

    fixed error caused by adding .js file into folder

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const filmDetails = document.querySelector('.film-details');  
-const filmsList = document.getElementById('films');  
+const filmDetails = document.querySelector('.film-details');
+const filmsList = document.getElementById('films');
 const filmPoster = document.getElementById("film-poster");
 const filmTitle = document.getElementById("film-title");
 const filmRuntime = document.getElementById("film-runtime");
@@ -7,23 +7,30 @@ const filmShowtime = document.getElementById("film-showtime");
 const availableTickets = document.getElementById("film-tickets");
 const buyTicketBtn = document.getElementById("buy-ticket");
 
-const filmsUrl = 'http://localhost:3001/films';
+const filmsUrl = 'http://localhost:3002/films';
 
+fetchFirstMovie();
+fetchAllFilms();
 
+//function to fetch placeholder film
 function fetchFirstMovie() {
     fetch(`${filmsUrl}/1`)
         .then(response => response.json())
         .then(film => {
+            if (!film.tickets_sold) {
+                film.tickets_sold = 0;
+            }
             displayFilmDetails(film);
             buyTicketBtn.onclick = () => buyTicketHandler(film);
-        });
+        })
+        .catch(error => console.error('Error fetching the first movie:', error));
 }
-
+//function to fetch all films
 function fetchAllFilms() {
     fetch(filmsUrl)
         .then(response => response.json())
         .then(films => {
-            filmsList.textContent = "";
+            filmsList.textContent = ""; 
             films.forEach(film => {
                 const filmsListItem = document.createElement('li');
                 filmsListItem.textContent = film.title;
@@ -34,7 +41,8 @@ function fetchAllFilms() {
                 });
                 filmsList.appendChild(filmsListItem);
             });
-        });
+        })
+        .catch(error => console.error('Error fetching all films:', error));
 }
 
 function displayFilmDetails(film) {
@@ -47,11 +55,12 @@ function displayFilmDetails(film) {
     buyTicketBtn.disabled = film.tickets_sold >= film.capacity;
 }
 
+//function to adress event handling
 function buyTicketHandler(film) {
     if (film.tickets_sold < film.capacity) {
         film.tickets_sold++;
         updateTicketsDisplay(film);
-        if (film.tickets_sold === film.capacity) {
+        if (film.tickets_sold > film.capacity) {
             isSoldOut();
         }
         updateTicketsSold(film);
@@ -74,9 +83,6 @@ function updateTicketsSold(film) {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({ tickets_sold: film.tickets_sold })
-    });
+    })
+    .catch(error => console.error('Error updating tickets sold:', error));
 }
-
-
-
- 
